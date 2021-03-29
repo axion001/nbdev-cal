88af938d3557bec04fce84266ad7c517702e1c50
Axion <axion001@protonmail.com>
2021-03-28 17:37:23 -0700

    initial upload of codebase

---
+++ procedures.js
@@ -0,0 +1,98 @@
+ const procedures ={
+    procedures: [
+        {
+            _id: '1' ,
+            name: 'P.D.E',
+            category: 'meditation',
+        },
+        { 
+            _id: '2',
+            name: 'Coffee Enema',
+            category: 'Coffee',
+        },
+        {    _id: '3',
+            name: 'Mouth Coffee Hold',
+            category: 'Coffee',
+        },
+        {
+            _id: '4',
+            name: 'Neti',
+            category: 'Coffee',
+        },
+        {
+            _id: '5',
+            name: 'Salt Rub',
+            category: 'Skin',
+        },
+        {
+            _id: '6',
+            name: 'Skin Brushing',
+            category: 'Skin',
+        },
+        {
+            _id: '7',
+            name: 'Kelp Wrap',
+            category: 'Skin',
+        },
+        {
+            _id: '8',
+            name: 'Tapping',
+            category: 'Reflex Points',
+        },
+        {
+            _id: '9',
+            name: 'Foot Refloxology',
+            category: 'Reflex Points',
+        },
+        {
+            _id: '10',
+            name: 'Sauna',
+            category: 'Sauna',
+        },
+        {
+            _id: '11',
+            name: 'Single Spot Light',
+            category: 'Sauna',
+        },
+        {
+            _id: '12',
+            name: 'Bidet',
+            category: 'Reflex Points',
+        },
+        {
+            _id: '13',
+            name: 'Trance',
+            category: 'Trance',
+        },
+        {
+            _id: '14',
+            name: 'V. Coffee Implant',
+            category: 'Vagina',
+        },
+        {
+            _id: '15',
+            name: 'V. Peroxide Implant',
+            category: 'Vagina',
+        },
+        {
+            _id: '16', 
+            name: 'V. Ozone Implant',
+            category: 'Vagina',
+        },
+        {
+            _id: '17',
+            name: 'Genital Bath',
+            category: 'Vagina',
+        },
+        {
+            _id: '18',
+            name: 'Insufflation',
+            category: 'Ozone',
+        },
+        {
+            _id: '19',
+            name: 'Down Sex',
+            category: 'Meditation',
+        },
+    ],
+};
